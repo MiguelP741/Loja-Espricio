19642c97efcb94240f9ede1dceae0a2611167e39
unknown <miguelaraujo.fox05@gmail.com>
2025-10-07 15:35:37 -0300

    Atualização 07/10

--- src/models/produtoModel.js
+++ src/models/produtoModel.js
@@ -16,6 +16,24 @@ const produtoModel = {
             console.error('Erro ao buscar produtos', error);
             throw error;// passa o erro para o controller tratar
         }
+    },
+
+    inserirProduto: async (nomeProduto, precoProduto)=>{
+        try {
+            
+            const pool = await getConnection();
+
+            let querySQL = 'INSERT INTO Produtos(nomeProduto, precoProduto) VALUES (@nomeProduto, @precoProduto)';
+
+            await pool.request()
+            .input('nomeProduto', sql.VarChar(100), nomeProduto)
+            .input('precoProduto', sql.Decimal(10,2), precoProduto)
+            .query(querySQL);
+
+        } catch (error) {
+            console.error('Erro ao inserir produto:',error);
+            throw error;// passa o erro para o controller tratar
+        }
     }
 }
 
